6e3cb6f19b012b5b0296ee650ef132e310551477
user.name <anafelix0909@gmail.com>
2025-06-05 21:55:35 -0300

    Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1,"ana");
-		System.out.println(dp);
+		Department dp = new Department(1,"TI");
+		
+		Seller s = new Seller(21, "ANA", "ANA@2123", new Date(), 3000.00,dp);
+		System.out.println(s);
 	}
 
 }
