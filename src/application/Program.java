48ea07e607d8dae3a7c0e916a84dc420300d02f1
user.name <anafelix0909@gmail.com>
2025-06-06 00:53:49 -0300

    Criando o metodo findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,10 +17,17 @@ public class Program {
 		System.out.println("=== Test 1: Seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		System.out.println();
 		
 		System.out.println("=== Test 1: Seller findByDepartment ===");
 		Department dep = new Department(2,null);//como meu metodo precisa de um department foi feito essa instancia normal so para pode pa
 		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		list.forEach(System.out::println);
+		System.out.println();
+		
+		System.out.println("=== Test 1: Seller findByAll ===");
+		list = sellerDao.findAll();
 		
 		list.forEach(System.out::println);
 
