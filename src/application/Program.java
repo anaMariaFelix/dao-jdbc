9f2462002a27bd303797d0b740bbdbcf84906362
user.name <anafelix0909@gmail.com>
2025-06-05 21:37:45 -0300

    Department entities class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department dp = new Department(1,"ana");
+		System.out.println(dp);
+	}
+
+}
